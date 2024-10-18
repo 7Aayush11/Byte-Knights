39b2e42773fb5d21ecb4a05346a82e0ec8001f08
7Aayush11 <nisarayush1172004@gmail.com>
2024-10-18 22:25:41 +0530

    Fourth Commit

---
+++ templates/about.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+document.querySelectorAll('.timeline-item').forEach((item) => {
+    observer.observe(item);
+});
